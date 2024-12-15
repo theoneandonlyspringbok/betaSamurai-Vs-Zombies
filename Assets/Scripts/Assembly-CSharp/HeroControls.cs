82e8ba14820678cdc30f9779a7d3d570a5e40540
EnderPurrson <enderlord9990@gmail.com>
2024-12-14 23:05:15 -0500

    pc controls

--- Assets/Scripts/Assembly-CSharp/HeroControls.cs
+++ Assets/Scripts/Assembly-CSharp/HeroControls.cs
@@ -16,7 +16,66 @@ public class HeroControls
 
 	private bool mWasMoving;
 
-	public void Update()
+	private KeyCode mCurrentKey = KeyCode.A, mHeldKey = KeyCode.None;
+
+	private void UpdatePCControls()
+	{
+		//flips the else statements depending on the key you last pressed
+		//this is so if you're holding d and you press a, you'll go backwards. and vice versa. instead of d just taking priority over a.
+		//I did this in brawlers as well. it makes the controls feel a lot less clunky.
+		if (mCurrentKey == KeyCode.A)
+		{
+			if (Input.GetKey(KeyCode.D))
+			{
+				onMoveRight();
+
+				if (!Input.GetKey(mCurrentKey))
+				{
+					mCurrentKey = KeyCode.D;
+				}
+			}
+			else if (Input.GetKey(KeyCode.A))
+			{
+				onMoveLeft();
+				
+				if (!Input.GetKey(mCurrentKey))
+				{
+					mCurrentKey = KeyCode.A;
+				}
+			}
+			else
+			{
+				onDontMove();
+			}
+		}
+		else if (mCurrentKey == KeyCode.D)
+		{
+			if (Input.GetKey(KeyCode.A))
+			{
+				onMoveLeft();
+				
+				if (!Input.GetKey(mCurrentKey))
+				{
+					mCurrentKey = KeyCode.A;
+				}
+			}
+			else if (Input.GetKey(KeyCode.D))
+			{
+				onMoveRight();
+
+				if (!Input.GetKey(mCurrentKey))
+				{
+					mCurrentKey = KeyCode.D;
+				}
+			}
+			else
+			{
+				onDontMove();
+			}
+		}
+	}
+
+	private void UpdateMobileControls()
 	{
 		int num = 0;
 		if (WeakGlobalInstance<SUIScreen>.instance.inputs.justTouched)
@@ -65,6 +124,18 @@ public class HeroControls
 		else if (onDontMove != null)
 		{
 			onDontMove();
+		}
+	}
+
+	public void Update()
+	{
+		if (Application.isMobilePlatform)
+		{
+			UpdateMobileControls();
+		}
+		else
+		{
+			UpdatePCControls();
 		}
 	}
 }
